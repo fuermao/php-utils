04dcd3d25eca0d0ecbfabb2ac2608244394434d8
ermao <120191115@qq.com>
2020-02-28 09:10:58 +0800

    添加字符串工具类

---
+++ test/PhpUnitTestBootStrap.php
@@ -0,0 +1,3 @@
+<?php
+define("Test_DS",DIRECTORY_SEPARATOR);
+require ( dirname(__DIR__).Test_DS."vendor".Test_DS."autoload.php");
---
+++ test/StringUtilsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace YiChUtilsTest;
+
+use YiChUtils\StringUtils;
+use PHPUnit\Framework\TestCase;
+
+class StringUtilsTest extends TestCase
+{
+	
+	public function testRandomString()
+	{
+		echo 11;
+	}
+}
